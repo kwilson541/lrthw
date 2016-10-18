437844280746e57f891c67a1508303cd00278a5c
Kim Wilson <k.wilson541@gmail.com>
2016-10-18 17:03:09 +0100

    Added Exercises 1-3

---
+++ ex02.rb
@@ -0,0 +1,9 @@
+# A comment, this is so that you can read the program later
+# Anything after the # is ignored by Ruby
+
+puts "I could have code like this." # And the comment after is ignored
+
+# You can also se a comment to "disable" or comment out a piece of code
+# puts "This won't run"
+
+puts "This will run"
