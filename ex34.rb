20016da65a817c6927a989f9542e8e72b1358e49
Kim Wilson <k.wilson541@gmail.com>
2016-10-23 13:41:50 +0100

    Added Exercise 34

---
+++ ex34.rb
@@ -0,0 +1,38 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1.
+# ruby
+
+# The third (3rd) animal.
+# peacock
+
+# The first (1st) animal.
+# bear
+
+# The animal at 3.
+# kangaroo
+
+# The fifth (5th) animal.
+# whale
+
+# The animal at 2.
+# peacock
+
+# The sixth (6th) animal.
+# platypus
+
+# The animal at 4.
+# whale
+
+# The first animal is bear at 0
+puts animals[0]
+# The second animal is ruby at 1
+puts animals[1]
+# The third animal is peacock at 2
+puts animals[2]
+# The fourth animal is kangaroo at 3
+puts animals[3]
+# The fifth animal is whale at 4
+puts animals[4]
+# The sixth animal is platypus at 5
+puts animals[5]
